542e537d94bc4e94509e6983f9593fb2da476571
joshuashumshere <j.leslie8@gmail.com>
2023-07-28 12:03:35 +0000

    Add hmtl and css

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # create an object of the class 
 
 @app.route("/") #path of the url after the url
 
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)

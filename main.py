af59a20b6a1bf2050052683c6b1c5982c316c10c
joshuashumshere <j.leslie8@gmail.com>
2023-08-04 09:24:15 +0000

    added css details

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__) # create an object of the class 
+app = Flask(__name__)  # create an object of the class
 
-@app.route("/") #path of the url after the url
 
+@app.route("/")  #path of the url after the url
 def hello_world():
-  return render_template('home.html')
+  return render_template('home_css_bootstrap.html')
+  #render_template('home.html') # this was with original CSS that was manually input
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
